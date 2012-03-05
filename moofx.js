d94d960d0a85039efa19f1ee6e4d27f0b5865631
Valerio Proietti <kamicane@gmail.com>
2012-03-05 15:43:17 +0100

    - 3.0.3

--- moofx.js
+++ moofx.js
@@ -1,7 +1,7 @@
 /*
 ---
 provides: moofx
-version: 3.0.2-8
+version: 3.0.3
 description: A CSS3-enabled javascript animation library on caffeine
 homepage: http://moofx.it
 author: Valerio Proietti <@kamicane> (http://mad4milk.net)
