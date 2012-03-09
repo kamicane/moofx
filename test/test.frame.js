591e6b4f11d7837aa121868a351da7f767972373
Valerio Proietti <kamicane@gmail.com>
2012-03-09 02:55:55 +0100

    - fixed race condition in requestFrame / cancelFrame.  - added relevant spec.  - cancelFrame now only cancels the first occurrence of the same callback. added a spec, and adjusted specs accordingly.

--- test/test.frame.js
+++ test/test.frame.js
@@ -1,46 +1,101 @@
 describe('requestFrame/cancelFrame', function (){
 
 	it('should get the same time argument', function(done){
-		
+
 		var time = null
 		var length = 0
-	
+
 		var next = function(now){
 			length++
 			if (time == null) time = now
 			if (time != now) isSuccess = false
 			if (length == 4) done()
 		}
-	
+
 		moofx.requestFrame(next)
 		moofx.requestFrame(next)
 		moofx.requestFrame(next)
 		moofx.requestFrame(next)
 	})
-	
+
+	it('should only cancel one callback', function(done){
+
+		var iter = 0
+
+		var next = function(){
+			++iter
+		}
+
+		moofx.requestFrame(next)
+		moofx.requestFrame(next)
+		moofx.requestFrame(next)
+		moofx.requestFrame(next)
+
+		moofx.cancelFrame(next)
+
+		moofx.requestFrame(function(){
+			expect(iter).to.be(3)
+			done()
+		})
+
+
+	})
+
 	it('should cancel the requestFrame(s)', function(done){
-		
+
 		var isSuccess = true
-			
+
 		var next = function(){
 			isSuccess = false
 		}
-			
+
+		var next2 = function(){
+			isSuccess = false
+		}
+
 		moofx.requestFrame(next)
 		moofx.requestFrame(next)
-		moofx.requestFrame(next)
-		moofx.requestFrame(next)
-		
+		moofx.requestFrame(next2)
+		moofx.requestFrame(next2)
+
 		moofx.cancelFrame(next)
 		moofx.cancelFrame(next)
-		moofx.cancelFrame(next)
-		moofx.cancelFrame(next)
-		
-		setTimeout(function(){
+		moofx.cancelFrame(next2)
+		moofx.cancelFrame(next2)
+
+
+		moofx.requestFrame(function(){
 			expect(isSuccess).to.be(true)
 			done()
-		}, 500)
+		})
+
 
 	})
-	
+
+	it('should not have a race condition :~)', function(done){
+
+		var nextCalled = false
+		var next2Called = false
+
+		var next = function(now){
+			nextCalled = true
+			moofx.cancelFrame(next2)
+		}
+
+		var next2 = function(now){
+			next2Called = true
+		}
+
+		var next3 = function(){
+			expect(nextCalled).to.be(true)
+			expect(next2Called).to.be(false)
+			done()
+		}
+
+		moofx.requestFrame(next)
+		moofx.requestFrame(next2)
+		moofx.requestFrame(next3)
+
+	})
+
 })
